0601222011d3eb733804d5666fa339ea484f1ef9
maroo <maroo@gmail.com>
2023-01-27 18:42:07 +0630

    add kafka services

--- app/conf/config.go
+++ app/conf/config.go
@@ -1,3 +1,44 @@
 package conf
 
-func init() {}
+import (
+	"gopkg.in/yaml.v2"
+	"kafka-demoset/app/internal/logger"
+	"os"
+)
+
+type (
+	kafka struct {
+		Addr string `yaml:"addr"`
+	}
+
+	telegram struct {
+		TokenID string `yaml:"tokenID"`
+		GroupID string `yaml:"groupID"`
+	}
+)
+
+var (
+	_c struct {
+		Kafka    kafka    `yaml:"kafka"`
+		Telegram telegram `yaml:"telegram"`
+	}
+)
+
+func init() {
+	data, err := os.ReadFile("./conf/config.yaml")
+	if err != nil {
+		logger.Sugar.Error(err)
+	}
+
+	if err := yaml.Unmarshal(data, &_c); err != nil {
+		logger.Sugar.Error(err)
+	}
+}
+
+func Kafka() *kafka {
+	return &_c.Kafka
+}
+
+func Telegram() *telegram {
+	return &_c.Telegram
+}
